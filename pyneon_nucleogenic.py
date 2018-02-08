364ba67d9171763d0a5dc7e033a5fb169e0c5597
stephen-e-cox <cox.stephen.e@gmail.com>
2018-02-07 20:28:27 -0500

    Moved nucleogenic into own folder

--- pyneon_nucleogenic.py
+++
@@ -1,6 +0,0 @@
-from neon_yield import yieldcalc
-from cross_section import xsection_plot
-
-yieldinstance = yieldcalc('Aragonite', 'O18', 0.4795, 0.002012405, 15.9994)
-
-yieldplot = xsection_plot('O18')
